b6feb8fb3a681298d05e0a3119dddfa907a5548c
kaili211 <k-zhu@mercari.com>
2020-07-19 22:43:25 +0900

    update

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.datasets import fashion_mnist
 
 
-def define_model():
-    n_classes = 10
+def define_model(n_classes):
     model = tf.keras.Sequential([
         tf.keras.layers.Conv2D(32, 5, activation=tf.nn.relu, input_shape=(28, 28, 1)),
         tf.keras.layers.MaxPool2D(2, 2),
@@ -17,34 +16,25 @@ def define_model():
 
     model.summary()
 
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(1e-5),
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
-    )
-
     return model
 
 
-def prepare_dataset():
+def load_data():
     (train_x, train_y), (test_x, test_y) = fashion_mnist.load_data()
-
     # Scale input in [-1, 1] range
-    train_x = train_x / 255. * 2 - 1
-    test_x = test_x / 255. * 2 - 1
+    train_x = tf.expand_dims(train_x, -1)
+    train_x = (tf.image.convert_image_dtype(train_x, tf.float32) - 0.5) * 2
+    train_y = tf.expand_dims(train_y, -1)
 
-    # Add the last 1 dimension, so to have images 28x28x1
-    train_x = tf.expand_dims(train_x, -1).numpy()
-    test_x = tf.expand_dims(test_x, -1).numpy()
+    test_x = tf.expand_dims(test_x, -1)
+    test_x = (tf.image.convert_image_dtype(test_x, tf.float32) - 0.5) * 2
+    test_y = tf.expand_dims(test_y, -1)
 
-    return train_x, train_y, test_x, test_y
+    return (train_x, train_y), (test_x, test_y)
 
 
 def train():
-    model = define_model()
-    train_x, train_y, test_x, test_y = prepare_dataset()
-    model.fit(train_x, train_y, epochs=10)
-    model.evaluate(test_x, test_y)
+    pass
 
 
 train()
