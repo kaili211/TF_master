3f9af7ea7072e761aaea4906e663b8f15c8a8634
kaili211 <k-zhu@mercari.com>
2020-07-22 22:11:40 +0900

    update

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 
 class Config(object):
     def __init__(self, output_dir, epochs, batch_size, log_every_step, v1=False, **kwargs):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
         if v1:
             self.ckpt_dir_train = os.path.join(output_dir, 'ckpt_train')
             self.ckpt_dir_dev = os.path.join(output_dir, 'ckpt_dev')
